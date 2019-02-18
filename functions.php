1da29e10ca67e3a1ca23c4951a1926d8409a0d78
Stepan <wwwstepanwww@mail.ru>
2019-02-18 20:32:22 +0300

    MySQL Испаравления

--- functions.php
+++ functions.php
@@ -1,10 +1,11 @@
 <?php
-function include_template($name, $data) {
+function include_template($name, $data)
+{
     $name = 'templates/' . $name;
     $result = '';
 
     if (!is_readable($name)) {
-    return $result;
+        return $result;
     }
 
     ob_start();
@@ -16,7 +17,8 @@ function include_template($name, $data) {
     return $result;
 }
 
-function format($number) {
+function format($number)
+{
     $number = ceil($number);
     if ($number >= 1000) {
         return number_format($number, 0, null, ' ') . ' &#8381;';
@@ -24,11 +26,44 @@ function format($number) {
     return $number . ' &#8381;';
 }
 
-function time_tomorrow() {
+function time_tomorrow()
+{
     $ts_midnight = strtotime('tomorrow midnight');
     $secs_to_midnight = $ts_midnight - time();
     $hours = floor($secs_to_midnight / 3600);
     $minutes = floor(($secs_to_midnight % 3600) / 60);
-    $result = sprintf( '%02d:%02d', $hours, $minutes) ;
+    $result = sprintf('%02d:%02d', $hours, $minutes);
     return $result;
+}
+
+function db_connect($db_config)
+{
+    $connection = mysqli_connect($db_config['host'], $db_config['user'], $db_config['password'], $db_config['database']);
+
+    if (!$connection) {
+        die('Ошибка подключение: ' . mysqli_connect_error());
+    }
+    mysqli_set_charset($connection, "utf8");
+    return $connection;
+
+}
+
+function get_categories($connection) {
+    $sql = 'SELECT `id`, `name` FROM categories';
+    $result = mysqli_query($connection, $sql);
+    $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    return $categories;
+}
+
+function get_lots($connection) {
+    $sql = '
+        SELECT lots.name, lots.start_price, lots.img, categories.name AS category_name
+        FROM lots
+        JOIN categories
+        ON lots.сategory_id = categories.id
+        ORDER BY lots.end_time DESC;
+    ';
+    $result = mysqli_query($connection, $sql);
+    $lots = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    return $lots;
 }
--- index.php
+++ index.php
@@ -1,79 +1,14 @@
 <?php
 date_default_timezone_set('Europe/Moscow');
 require_once('functions.php');
+$config = require('config.php');
 
 $is_auth = rand(0, 1);
 $user_name = 'Степан'; // укажите здесь ваше имя1
-/*
-$categories = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
 
-$lots = [
-    [
-        'name' => '2014 Rossignol District Snowboard',
-        'cate' => 'Доски и лыжи',
-        'price' => 10999,
-        'image' => 'img/lot-1.jpg'
-    ],
-    [
-        'name' => 'DC Ply Mens 2016/2017 Snowboard',
-        'cate' => 'Доски и лыжи',
-        'price' => 159999,
-        'image' => 'img/lot-2.jpg'
-    ],
-    [
-        'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-        'cate' => 'Крепления',
-        'price' => 8000,
-        'image' => 'img/lot-3.jpg'
-    ],
-    [
-        'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
-        'cate' => 'Ботинки',
-        'price' => 10999,
-        'image' => 'img/lot-4.jpg'
-    ],
-    [
-        'name' => 'Куртка для сноуборда DC Mutiny Charocal',
-        'cate' => 'Одежда',
-        'price' => 7500,
-        'image' => 'img/lot-5.jpg'
-    ],
-    [
-        'name' => 'Маска Oakley Canopy',
-        'cate' => 'Разное',
-        'price' => 5400,
-        'image' => 'img/lot-6.jpg'
-    ]
-];
-*/
-$connect = mysqli_connect('localhost', 'root','','yeticave');
-
-if (!$connect) {
-    print('Ошибка подключение: ' . mysqli_connect_error());
-}
-
-mysqli_set_charset($connect, 'utf8');
-
-$cat = 'SELECT id, name FROM categories';
-$result = mysqli_query($connect, $cat);
-$categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-
-$cat = '
-SELECT lots.name, lots.start_price, lots.img, MAX(bets.amount), categories.name AS category_name
-FROM lots
-JOIN categories
-ON lots.сategory_id = categories.id
-JOIN bets
-ON bets.lot_id = lots.id
-WHERE lots.winner_id = 1
-GROUP BY bets.lot_id
-ORDER BY lots.end_time DESC
-LIMIT 9';
-$result = mysqli_query($connect, $cat);
-$lots = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-
+$connection = db_connect($config['db']);
+$categories = get_categories($connection);
+$lots = get_lots($connection);
 
 $content = include_template('index.php', [
     'categories' => $categories,
