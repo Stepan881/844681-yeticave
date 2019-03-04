445a2d14cc51ac9ae281a2d0a47f8f1bc09220b5
Stepan <wwwstepanwww@mail.ru>
2019-03-04 18:30:54 +0300

    Регистрация пользователя в db

--- sign-up.php
+++ sign-up.php
@@ -26,12 +26,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $errors = validate_user($connection, $user_data);
 
   if (!$errors and !$user_avatar) {
+
     $user_data['avatar'] = (upload_img($file_data['tmp_name'], $file_data['name']));
     $user_data['password'] = password_hash($user_data['password'], PASSWORD_DEFAULT);
 
     $user_id = add_user($connection, $user_data);
-
     if ($user_id) {
+      $counter_name = "visitcount";
+
+
+
+
       header('Location: index.php');
       exit();
     }
